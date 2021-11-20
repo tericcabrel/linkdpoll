26dc1e8524dbfe5676ab09a73e4fdf1bf26c28e3
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2021-11-20 23:40:35 +0100

    feat: fix style import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 
-import styles from '@/styles/Home.module.css';
+import styles from '@/styles/home.module.css';
 import { PollGenerator } from '@/components/poll/poll-generator';
 
 export default function Home() {
