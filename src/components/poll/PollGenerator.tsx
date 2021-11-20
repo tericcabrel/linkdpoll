ff571fc36935d3092b1545a57567d381aecb5819
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2021-11-20 08:44:49 +0100

    chore: add reaction pictures

---
+++ src/components/poll/PollGenerator.tsx
@@ -0,0 +1,11 @@
+const PollGenerator = () => {
+  return (
+    <div>
+      <h1>
+        Welcome to <a href="https://nextjs.org">Next.js!</a>
+      </h1>
+    </div>
+  );
+};
+
+export { PollGenerator };
